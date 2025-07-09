7e336575fbf3b62710a6adcbe4f88297a3bb2cc1
aditya <adityabhise1111@gmail.com>
2025-07-09 19:45:48 +0530

    ninad

--- 1_basics/StringExample.java
+++ 1_basics/StringExample.java
@@ -1,6 +1,6 @@
 public class StringExample{
     public static void main(String[]  args){
-        String s= "Aditya bhise";
+        String s= "Aditya panduran bhise";
 
         // in java strings are immutable
         //lets try 
